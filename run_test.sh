1b19f861817c380155b9cc7daf48bb53bb3031c0
reilly <reilly.grant@gmail.com>
2018-02-06 22:03:00 -0600

    updated tests

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,8 @@
-./compiler.native test1.arith > tmp.txt~
-diff tmp.txt~ test1.out
-./compiler.native test2.arith > tmp.txt~
-diff tmp.txt~ test2.out
+#./compiler.native tests/test1.arith > tmp.txt~
+#diff tmp.txt~ tests/test1.out
+#./compiler.native tests/test2.arith > tmp.txt~
+#diff tmp.txt~ tests/test2.out
+#./compiler.native tests/test3.arith > tmp.txt~
+#diff tmp.txt~ tests/test3.out
+#./compiler.native tests/test4.arith > tmp.txt~
+#diff tmp.txt~ tests/test4.out
